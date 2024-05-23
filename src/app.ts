a405b4ffabbd834ffbf3223066dbe47e3ea9e676
Nemeth <Nemeth@ktch.hu>
2024-05-23 10:44:04 +0200

    .ts input elements

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,70 @@
-console.log("működik")
+class Romb{
+
+    sideInput?:HTMLInputElement | null
+    alphaInput?:HTMLInputElement | null
+    perimeterInput?:HTMLInputElement | null
+    areaInput?:HTMLInputElement | null
+    calcButton?:HTMLButtonElement | null
+
+
+    constructor(){
+        this.bindHTML()
+        this.handleEvent()
+    }
+    
+    
+    bindHTML(){
+        this.sideInput = document.querySelector("#side")
+        this.alphaInput = document.querySelector("#alpha")
+        this.perimeterInput = document.querySelector("#perimeter")
+        this.areaInput = document.querySelector("#area")
+        this.calcButton = document.querySelector("#calcButton")
+
+
+
+
+    }
+
+    handleEvent(){
+        this.calcButton?.addEventListener('click',()=>{
+                this.startCalc()
+        })
+    }
+
+
+    startCalc(){
+        const side = this.sideInput?.value
+        const alpha = this.alphaInput?.value
+        const perimeter= this.calcPerimeter(Number (side))     
+        const area = this.calcArea(Number(side), Number(alpha))
+        this.renderResult(perimeter, area)
+        
+        
+    }
+
+    calcPerimeter(side: number):number{
+        return side*4
+    }
+
+    calcArea(side:number, alpha:number):number{
+        const rad = alpha*Math.PI/180
+        
+        return Math.pow(side, 2)*Math.sin(rad)
+
+
+    }
+
+    renderResult(perimeter:number, area:number){
+        if(this.perimeterInput){
+            this.perimeterInput.value =String(perimeter) 
+
+        }
+        if(this.areaInput){
+            this.areaInput.value =String(area) 
+
+        }
+    }
+
+
+}
+new Romb()
